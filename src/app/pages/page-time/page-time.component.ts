4d45909902ad01db303cd8e5b679d9f4dcee08b1
lu1sHenrique <lhenrique.lh000@gmail.com>
2025-06-04 14:57:53 -0300

    feat: adicionando page de times

--- src/app/pages/page-time/page-time.component.ts
+++ src/app/pages/page-time/page-time.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TimeComponent } from "../../../shared/components/time/time.component";
+import { LoadingComponent } from '../../../shared/components/loading/loading.component';
+import { PageBreadcrumbHeaderComponent } from '../../../shared/components/page-breadcrumb-header/page-breadcrumb-header.component';
 
 @Component({
   selector: 'app-page-time',
-  imports: [],
+  imports: [TimeComponent, LoadingComponent, PageBreadcrumbHeaderComponent],
   templateUrl: './page-time.component.html',
   styleUrl: './page-time.component.scss'
 })
