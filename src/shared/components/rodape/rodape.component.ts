b33a37e003d7fc917466053b2e29af8dbc69154d
lu1sHenrique <lhenrique.lh000@gmail.com>
2025-06-04 11:46:18 -0300

    refactor: ajustando rodape

--- src/shared/components/rodape/rodape.component.ts
+++ src/shared/components/rodape/rodape.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rodape',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './rodape.component.html',
   styleUrl: './rodape.component.scss'
 })
