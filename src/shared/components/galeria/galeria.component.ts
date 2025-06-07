78a1e902dce0282087d966241b434f85e19f4096
lu1shenrique <lhenrique.lh000@gmail.com>
2025-06-07 11:37:50 -0300

    refactor: refatorando componente de galeria

--- src/shared/components/galeria/galeria.component.ts
+++ src/shared/components/galeria/galeria.component.ts
@@ -2,10 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
-  imports: [],
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrls: ['./galeria.component.scss']
 })
 export class GaleriaComponent {
+  modalVisible = false;
+  selectedImage = '';
 
+  images = [
+    { src: 'assets/img/gallery/gallery-1.jpg', wide: true },
+    { src: 'assets/img/gallery/gallery-7.jpg', wide: false },
+    { src: 'assets/img/gallery/gallery-11.jpg', wide: false },
+    { src: 'assets/img/gallery/gallery-8.jpg', wide: false },
+    { src: 'assets/img/gallery/gallery-10.jpg', wide: false },
+    { src: 'assets/img/gallery/gallery-2.jpg', wide: true }
+  ];
+
+  openModal(imageSrc: string) {
+    this.selectedImage = imageSrc;
+    this.modalVisible = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+    document.body.style.overflow = 'auto';
+  }
 }
