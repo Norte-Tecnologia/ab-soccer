fffe82049c429fe981f489771412927de76f17d9
lu1sHenrique <lhenrique.lh000@gmail.com>
2025-06-11 18:31:11 -0300

    feat: criando página de contato e componentes

--- src/app/pages/page-contato/page-contato.component.ts
+++ src/app/pages/page-contato/page-contato.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LoadingComponent } from '../../../shared/components/loading/loading.component';
+import { PageBreadcrumbHeaderComponent } from '../../../shared/components/page-breadcrumb-header/page-breadcrumb-header.component';
+import { FormContatoComponent } from '../../../shared/components/form-contato/form-contato.component';
+import { MapaLocalizacaoComponent } from '../../../shared/components/mapa-localizacao/mapa-localizacao.component';
 
 @Component({
   selector: 'app-page-contato',
-  imports: [],
+  imports: [LoadingComponent, PageBreadcrumbHeaderComponent, FormContatoComponent, MapaLocalizacaoComponent],
   templateUrl: './page-contato.component.html',
   styleUrl: './page-contato.component.scss'
 })
---
+++ src/shared/components/mapa-localizacao/mapa-localizacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-localizacao',
+  imports: [],
+  templateUrl: './mapa-localizacao.component.html',
+  styleUrl: './mapa-localizacao.component.scss'
+})
+export class MapaLocalizacaoComponent {
+
+}
