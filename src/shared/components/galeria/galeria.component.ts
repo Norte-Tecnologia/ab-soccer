9fe159f7922ad346d00c0212cc1b90de1c572141
lu1sHenrique <lhenrique.lh000@gmail.com>
2025-06-11 18:24:52 -0300

    fix: ajustando lógica de exibição de imagens em home e registros

--- src/shared/components/galeria/galeria.component.ts
+++ src/shared/components/galeria/galeria.component.ts
@@ -13,21 +13,21 @@ export class GaleriaComponent {
 
   images = [
     { src: 'assets/img/gallery/gallery-1.jpg', wide: true, exibir: true },
-    { src: 'assets/img/gallery/gallery-3.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-4.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-5.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-6.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-7.jpg', wide: false, exibir: true },
-    { src: 'assets/img/gallery/gallery-9.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-12.jpg', wide: true, exibir: this.pageRegistros },
+    { src: 'assets/img/gallery/gallery-3.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-4.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-5.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-6.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-9.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-12.jpg', wide: true, exibir: false },
     { src: 'assets/img/gallery/gallery-11.jpg', wide: false, exibir: true },
     { src: 'assets/img/gallery/gallery-8.jpg', wide: false, exibir: true },
     { src: 'assets/img/gallery/gallery-10.jpg', wide: false, exibir: true },
+    { src: 'assets/img/gallery/gallery-7.jpg', wide: false, exibir: true },
     { src: 'assets/img/gallery/gallery-13.jpg', wide: true, exibir: true },
-    { src: 'assets/img/gallery/gallery-14.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-15.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-16.jpg', wide: true, exibir: this.pageRegistros },
-    { src: 'assets/img/gallery/gallery-17.jpg', wide: true, exibir: this.pageRegistros },
+    { src: 'assets/img/gallery/gallery-14.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-15.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-16.jpg', wide: true, exibir: false },
+    { src: 'assets/img/gallery/gallery-17.jpg', wide: true, exibir: false },
   ];
 
   openModal(imageSrc: string) {
