fb73a2d731eca867d886c8d457a41a9ee4350dcc
lu1sHenrique <lhenrique.lh000@gmail.com>
2025-06-12 17:18:50 -0300

    refactor: ajustando animação de barra e refatorando

--- src/shared/components/o-que-fazemos/o-que-fazemos.component.ts
+++ src/shared/components/o-que-fazemos/o-que-fazemos.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-o-que-fazemos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './o-que-fazemos.component.html',
-  styleUrl: './o-que-fazemos.component.scss'
+  styleUrls: ['./o-que-fazemos.component.scss']
 })
 export class OQueFazemosComponent {
-
+  progressValues = [
+    { id: 'bar1', value: 100, text: 'Desenvolvimento físico' },
+    { id: 'bar2', value: 100, text: 'Desenvolvimento técnico' },
+    { id: 'bar3', value: 100, text: 'Acompanhamento completo' }
+  ];
 }
